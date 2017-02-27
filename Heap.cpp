cae8fc1620278736fbdb78bad964f0c94b452957
Joshua-He <joshua5089@gmail.com>
2017-02-27 14:22:13 -0800

    Add files via upload

---
+++ Heap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+
+  cout << "Hello, don't look at this" << endl;
+  cout << "This code is not for your eyes" << endl;
+
+
+
+
+
+}
