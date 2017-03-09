c28c27d58e45777a086dbd866e0850e890f63a6c
Joshua-He <joshua5089@gmail.com>
2017-03-09 12:40:32 -0800

    Add files via upload

--- Heap.cpp
+++ Heap.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 #include <string.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(){
-
   int array[100];
+  char array1[205];
+  char temp[10] = "";
+  int arraySize = 0;
   int length;
 
-  cout << "Enter!..!..!..!..!..!..!" << endl;
+  
+  cout << "Hello" << endl;
+  cin.getline(array1, 205);
+  length = strlen(array1);
+  for (int i = 0; i <= length; i++){
+    if(array1[i] != ' ' && array1[i] != '\0'){
+      strncat(temp, array1 + i, 1);
+    }
+    else{
+      int x = atoi(temp);
+      temp[0] = '\0';
+      array[arraySize] = x;
+      arraySize++;
+    }
+  }
+  cout << "Length" << arraySize << endl;
+  for (int i = 0; i < arraySize; i++){
+    cout << array[i] << endl;
+  }
   
 
-
-
-
-
+  
 }
 
-void maxHeapify(){
-
+void printTree(int size, int array[100]){
+  
 }
